bb0b0de8d0b1862670ec3afa12902cf2f51979f6
Denis Y. Spichkin <ds@levellab.ru>
2016-02-10 13:59:54 +0300

    fix

--- src/app/devices/devices.controller.js
+++ src/app/devices/devices.controller.js
@@ -5,9 +5,23 @@
     .module('devices')
     .controller('DevicesController', DevicesController);
 
-  function DevicesController(devices) {
+  function DevicesController($userdeviceservice, $deviceservice) {
     var vm = this;
 
-    vm.devices = devices.devices;
+    var users = $userdeviceservice.get();
+    var selected_devices = {};
+    var devices = [];
+    for (var i = 0, len = users.length; i < len; i++) {
+    	if (users[i].hasOwnProperty('selected') && users[i].selected === true) {
+    		for (var j = 0, lenj = users[i].devices.length; j < lenj; j++) {
+    			selected_devices[users[i].devices[j]] = true;
+    		}
+    	}
+    }
+
+    for (var x in selected_devices) {
+    	devices.push($deviceservice.get_device_by_id(x));
+    }
+    vm.devices = devices;
   }
 })();
